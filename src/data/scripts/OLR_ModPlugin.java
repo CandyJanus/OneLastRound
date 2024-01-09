46696b8951dccf9b9575d359b9b1f1be617dc8ad
Candy <85752963+CandyJanus@users.noreply.github.com>
2024-01-09 13:07:37 -0800

    Initial commit

---
+++ src/data/scripts/OLR_ModPlugin.java
@@ -0,0 +1,50 @@
+package data.scripts;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.sun.xml.internal.bind.v2.model.annotation.Quick;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.Method;
+
+public class OLR_ModPlugin extends BaseModPlugin {
+    @Override
+    public void onApplicationLoad() throws Exception {
+        super.onApplicationLoad();
+        chart();
+        // Test that the .jar is loaded and working, using the most obnoxious way possible.
+//        throw new RuntimeException("Template mod loaded! Remove this crash in TemplateModPlugin.");
+    }
+
+    @Override
+    public void onNewGame() {
+        super.onNewGame();
+
+        // The code below requires that Nexerelin is added as a library (not a dependency, it's only needed to compile the mod).
+//        boolean isNexerelinEnabled = Global.getSettings().getModManager().isModEnabled("nexerelin");
+
+//        if (!isNexerelinEnabled || SectorManager.getManager().isCorvusMode()) {
+//                    new MySectorGen().generate(Global.getSector());
+            // Add code that creates a new star system (will only run if Nexerelin's Random (corvus) mode is disabled).
+//        }
+    }
+
+    public static void chart() {
+
+        System.out.println("Hello world!");
+
+        double[] xData = new double[] { 0.0, 1.0, 2.0 };
+        double[] yData = new double[] { 2.0, 1.0, 0.0 };
+
+//        Method quikChart= Operations.class.getDeclaredMethod();
+//
+//        // Create Chart
+//        XYChart chart = MethodHandle(QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData));
+
+//        // Show it
+//        new SwingWrapper(chart).displayChart();
+    }
+}
+
