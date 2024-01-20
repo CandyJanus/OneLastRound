3ad5cd61158973ba8280770c895208b61e04ed35
Candy <85752963+CandyJanus@users.noreply.github.com>
2024-01-20 13:29:19 -0800

    emptied it all

--- src/data/scripts/OLR_ModPlugin.java
+++ src/data/scripts/OLR_ModPlugin.java
@@ -1,16 +1,12 @@
 package data.scripts;
 
 import com.fs.starfarer.api.BaseModPlugin;
-import org.knowm.xchart.QuickChart;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.XYChart;
 
 
 public class OLR_ModPlugin extends BaseModPlugin {
     @Override
     public void onApplicationLoad() throws Exception {
         super.onApplicationLoad();
-        chart();
         // Test that the .jar is loaded and working, using the most obnoxious way possible.
 //        throw new RuntimeException("Template mod loaded! Remove this crash in TemplateModPlugin.");
     }
@@ -26,20 +22,6 @@ public class OLR_ModPlugin extends BaseModPlugin {
 //                    new MySectorGen().generate(Global.getSector());
             // Add code that creates a new star system (will only run if Nexerelin's Random (corvus) mode is disabled).
 //        }
-    }
-
-    public static void chart() {
-
-        System.out.println("Hello world!");
-
-        double[] xData = new double[] { 0.0, 1.0, 2.0 };
-        double[] yData = new double[] { 2.0, 1.0, 0.0 };
-
-        // Create Chart
-        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-
-        // Show it
-        new SwingWrapper(chart).displayChart();
     }
 }
 
